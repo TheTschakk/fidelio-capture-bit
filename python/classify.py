b9c79df2b60d130bff56d10d1dbdbc3c4135c579
Jakob Staab <jakob.staab@yahoo.com>
2019-01-03 12:43:02 +0000

    fixed classify routine

--- python/classify.py
+++ python/classify.py
@@ -17,17 +17,19 @@ with open(path + 'log.txt') as f:
         if ':' in line:
             params = []
             r = 1
-            while ':' not in lines[i+r] and i+r < len(lines):
+            while ':' not in lines[i+r]:
                 params.append(lines[i+r])
                 r += 1
+                if i+r == len(lines)-1:
+                    break
             for param in params:
                 values = param.split(' ')
                 for j in range(len(values)):
-                    values[j] = float(values[i])
-                values = np.array(values)
+                    values[j] = float(values[j])
+                values = np.array([values])
                 group = SVC.clf.predict(values)
                 if group == 0:
-                    os.system('mv ' + path + line + ' ' + path + 'other/')
-                    break
+                    os.system('mv ' + path + line[:-1] + ' ' + path + 'others/')
                 else:
-                    os.system('mv ' + path + line + ' ' + path + 'meteors/')
+                    os.system('mv ' + path + line[:-1] + ' ' + path + 'meteors/')
+                    break
