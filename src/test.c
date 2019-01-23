2a4aff9023079cead97576aa6f5b26df48035237
Jakob Staab <jakob.staab@yahoo.com>
2019-01-23 19:20:48 +0000

    updated test program

--- src/test.c
+++ src/test.c
@@ -14,8 +14,8 @@
 #define SIZE (MAXPIX/INT)
 #define MAXMET 100
 
-//int limit = 50;
-int delta = 50;
+int delta = 100;
+int adj_rate = 150;
 int cutoff = 10;
 int depth = 2;
 int margins[4] = {10, WIDTH-10, 10, HEIGHT-10}; //left, right, top and bottom margine (currently all 10 px)
@@ -23,11 +23,12 @@ int margins[4] = {10, WIDTH-10, 10, HEIGHT-10}; //left, right, top and bottom ma
 static char *input;
 char cam_id = '0';
 const int buffer_size = 150;
-//static int prefluff = 25;
+static int prefluff = 25;
 static int postfluff = 25;
 
 void switchEle(int *list, int item1, int item2);
 void print1dArray(int *list, int dim);
+struct image *revertFrames(struct image *frm, int N);
 
 #include "met.h"
 #include "bits.h"
@@ -46,55 +47,41 @@ int mainloop(void) {
     int found=0;
     int lifetime;
 
-    struct timespec systime, reftime;
-
-    while (frm->index < (buffer_size - 1)) {
+    while (1) {
         //printf("frame %i ################################################\n", frm->index);
 
-        clock_gettime(CLOCK_REALTIME, &reftime);
-        analyseFrame(frm);
+        if (found == 0)
+            analyseFrame(frm);
 
-        //if ( frm->index == 1 )
-            //adjustSensitivity(frm, buffer_size, 30); 
+        if ( (frm->index % adj_rate) == 0 )
+            adjustSensitivity(frm, 10, 0);
 
-        //clock_gettime(CLOCK_REALTIME, &systime); printf("analyse Frame %f sec\n", (float) ((systime.tv_nsec - reftime.tv_nsec)/1000) / 1000000);
-
-        if ( endOfMeteor(frm, depth) != -1 ) {
-            found = lifetime;
-            printData(frm->prev->prev);
-            exit(0);
+        if ( endOfMeteor(frm, depth) && !found ) {
+	    lifetime = endOfMeteor(frm, depth);
+            found = 1;
         }
 
-        /*
-        */
-
-        if (found > 0)
+        if (found)
             n++;
-        else
-            n = 0;
 
         if (n > postfluff) {
-            //write_video(frm, (prefluff + found + postfluff));
+            printf("saving video, lifetime = %i\n", lifetime);
+            //write_video(frm, lifetime);
             n = 0;
             found = 0;
-            //sleep(5);
+	    lifetime = 0;
         }
 
-        //printImage(frm);
-
+        printImage(frm);
         frm = frm->next;
-
-        //if (frm->index == buffer_size)
-            //return 1;
     }
-    printData(frm->prev->prev);
+
     return 0;
 }
 
 
 int main(int argc,char* argv[]){
     input = argv[1];
-    buffer_size = argv[2];
     frm = buildBuffer(buffer_size); // generate cyclicalc buffer of size "buffer_size" frames
     read_video(input, frm); // invoke the read_video() function in order to fill generated buffer with frames from "input"
 
