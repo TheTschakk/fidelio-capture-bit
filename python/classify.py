1be4a938e43ee614ce14a824dc4295dbe2b206ba
SimonSkade <simon.skade@gmx.net>
2019-01-23 18:32:10 +0100

    fixed merge error

--- python/classify.py
+++ python/classify.py
@@ -17,17 +17,12 @@ with open(path + 'log.txt') as f:
             while ':' not in lines[i+r]:
                 params.append(lines[i+r])
                 r += 1
-<<<<<<< HEAD
                 if i+r == len(lines)-1:
-=======
-                if i+r == len(lines):
->>>>>>> 5c50c79973119fbae34e4e5c43d1fbd8c13a5348
                     break
             for param in params:
                 values = param.split(' ')
                 for j in range(len(values)):
                     values[j] = float(values[j])
-<<<<<<< HEAD
                     if values[j] == float('NaN') or values[j] == float('Inf'):
                         break
                 else:
@@ -38,13 +33,3 @@ with open(path + 'log.txt') as f:
                     else:
                         os.system('mv ' + path + line[:-1] + ' ' + path + 'meteors/')
                         break
-=======
-                values = np.array([values])
-                group = SVC.clf.predict(values)
-                if group == 0:
-                    if num == len(params)-1: #if all params are not meteors
-                        os.system('mv ' + path + line + ' ' + path + 'other/')
-                else:
-                    os.system('mv ' + path + line + ' ' + path + 'meteors/')
-                    break
->>>>>>> 5c50c79973119fbae34e4e5c43d1fbd8c13a5348
